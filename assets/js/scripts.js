cab3788497ad1af78bb5ea915272a4bf5c31cef6
Mary <xu.ruili99@gmail.com>
2018-06-07 05:15:59 -0700

    Update tabs and sticky nav bar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,4 +1,17 @@
-$('.nav-tabs a').click(function (e) {
-    // No e.preventDefault() here
-    $(this).tab('show');
-});
+// When the user scrolls the page, execute myFunction 
+window.onscroll = function() {stickyFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("nav-bar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function stickyFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
